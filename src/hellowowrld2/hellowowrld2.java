8a4c8a32e3170a482462c883f0018ab052b3120b
Ayca Tuzmen Yildirim <aycatuzmen@gmail.com>
2017-09-18 11:11:34 +0300

    updated string in println

--- src/hellowowrld2/hellowowrld2.java
+++ src/hellowowrld2/hellowowrld2.java
@@ -4,7 +4,7 @@ public class hellowowrld2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hellow AYCA 1 2 3 4 6 ");
+		System.out.println("hellow AYCA 1 2 3 4 6 7 8 ");
 	}
 
 }
