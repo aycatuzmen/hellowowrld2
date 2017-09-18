d3063754488227e2f4f85174335029f3ec93db0c
Ayca Tuzmen Yildirim <aycatuzmen@gmail.com>
2017-09-18 11:09:19 +0300

    Create HelloWorld project

---
+++ src/hellowowrld2/hellowowrld2.java
@@ -0,0 +1,10 @@
+package hellowowrld2;
+
+public class hellowowrld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow AYCA 1 2 3 4 6 ");
+	}
+
+}
